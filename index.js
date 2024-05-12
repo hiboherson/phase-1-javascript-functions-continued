8dad334e7acca60fabe5d0b28329bff1099b57bd
hiboherson <hiboherson@gmail.com>
2024-05-12 10:55:21 +0300

    complete javascript functions lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// code your solution here
+
+// index.js
+
+const mondayWork = function(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+}
+
+const wrapAdjective = function(symbol = '*') {
+    return function(adjective) {
+        return `You are ${symbol}${adjective}${symbol}!`;
+    }
+}
+
+module.exports = { saturdayFun, mondayWork, wrapAdjective };
+
+
+// index.js
+
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+module.exports = saturdayFun;
+
+
+
+
+
+
